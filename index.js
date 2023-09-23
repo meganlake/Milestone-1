a12d6aeed2d9ac93e7d3eccdb9d1ba114cc8c158
Megan Lake <meganlake95@gmail.com>
2023-09-23 14:22:35 -0700

    added counter to update score container

--- index.js
+++ index.js
@@ -1,21 +1,38 @@
 function wrongAnswer (evt) {
-    evt.target.classList.toggle("wrong-answer");
+    evt.target.classList.add("wrong-answer");
 }
 
 function correctAnswer (evt) {
-    evt.target.classList.toggle("correct-answer");
+    evt.target.classList.add("correct-answer");
 }
+
+let counter = 0;
+
+function addToCounter () {
+    counter += 1; 
+    console.log(counter);
+    document.querySelector('#score').innerHTML = counter;
+}
+
+let questionContainer = document.querySelectorAll(".question-container");
 
 let wrongList = document.querySelectorAll(".wrong");
 
 for (let i=0; i< wrongList.length; i++) {
     wrongList[i].addEventListener("click", wrongAnswer);
+    wrongList[i].addEventListener("click", 
+        () => {questionContainer[i].style.pointerEvents = "none"});
 }
 
 let correctList = document.querySelectorAll(".correct")
 
 for (let i=0; i< correctList.length; i++) {
     correctList[i].addEventListener("click", correctAnswer);
-    
+    correctList[i].addEventListener("click", addToCounter);
+    correctList[i].addEventListener("click", 
+        () => {questionContainer[i].style.pointerEvents = "none"});
 }
 
+// fix wrongList questionContainer code
+// add start over button
+// write instructions in the read me
